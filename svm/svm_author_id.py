5f77a21378fc68ca2874b701515bf37dfea839f4
MarcoPotok <potok.marco@gmail.com>
2016-12-15 20:06:14 +0100

    svm mini-project

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -23,8 +23,35 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 
 #########################################################
-### your code goes here ###
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+
+# features_train = features_train[:len(features_train)//100]
+# labels_train = labels_train[:len(labels_train)//100]
+
+# for x in range(1,5):
+c = 10**5
+
+clf = svm.SVC(kernel='rbf', C=c)
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print("training time:", round(time()-t0, 3), "s")
+
+t0 = time()
+pred = clf.predict(features_test)
+print("testing time:", round(time()-t0, 3), "s")
+
+print("C =", c , ":",accuracy_score(labels_test, pred))
+
+# print("Element 10: ", clf.predict(features_test[10]))
+# print("Element 26: ", clf.predict(features_test[26]))
+# print("Element 50: ", clf.predict(features_test[50]))
+
+print("Chris prediction number:", sum(pred), len(pred))
 
 #########################################################
-
+# training time: 141.468 s
+# testing time: 14.395 s
+# 0.984072810011
 
