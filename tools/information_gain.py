863521d32fcaceaa82f48aed5c55309e083490b5
MarcoPotok <potok.marco@gmail.com>
2016-12-16 23:08:02 +0100

    Fix information_gain

--- tools/information_gain.py
+++ tools/information_gain.py
@@ -1,14 +1,32 @@
 import math
 
-def information_gain(parent_entropy, children_occurrences):
+def get(parent_entropy, children_splitting):
 
-    tot_occurr = sum(children_occurrences)
-    children_entropy = 0
+    occurr = list_occurrences(children_splitting)
+    weighted_entropy = 0
 
-    for i in range(0, len(children_occurrences)):
-        p_i = children_occurrences[i] / tot_occurr
-        children_entropy += -(p_i * math.log2(p_i))*children_occurrences[i]
+    for i in range(0, len(children_splitting)):
+       e = entropy(children_splitting[i], occurr[i])
+       weighted_entropy += e * occurr[i]
 
-    children_entropy /= tot_occurr
+    weighted_entropy /= sum(occurr)
+    return parent_entropy - weighted_entropy
 
-    return 1 - children_entropy
+
+def entropy(frequency_list, occurr):
+    tot_occurr = occurr
+    entropy = 0
+
+    for c in frequency_list:   
+        if c < 1: continue
+        prob_c = c / tot_occurr
+        entropy -= (prob_c * math.log2(prob_c))
+    
+    return entropy
+
+
+def list_occurrences(list_of_lists):
+    summa = []
+    for l in list_of_lists:
+        summa.append(sum(l))
+    return summa
